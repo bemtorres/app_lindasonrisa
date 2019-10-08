40f49b2357e3ed32e8eb6c60bd2b82d5e2010919
Bemtorres <benja.mora.torres@gmail.com>
2019-10-08 01:21:13 -0300

    login

--- app/src/main/java/win/bemtorres/lindasonrisaproveedor/MainActivity.kt
+++ app/src/main/java/win/bemtorres/lindasonrisaproveedor/MainActivity.kt
@@ -1,12 +1,34 @@
 package win.bemtorres.lindasonrisaproveedor
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        comenzar_animaciones()
+
+
+    }
+    fun comenzar_animaciones(){
+        val animacion = AnimationUtils.loadAnimation(this,R.anim.lefttoright)
+
+        imgLogo.startAnimation(animacion!!)
+        textUsuario.startAnimation(animacion!!)
+        txtUsername.startAnimation(animacion!!)
+        textPass.startAnimation(animacion!!)
+        txtPassword.startAnimation(animacion!!)
+        btnIngresar.startAnimation(animacion!!)
     }
 }
