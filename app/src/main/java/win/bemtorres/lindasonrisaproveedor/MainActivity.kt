9025d5afdd86906021fb19363a8fa8913bff4319
Bemtorres <benja.mora.torres@gmail.com>
2019-10-07 22:56:25 -0300

    update

---
+++ app/src/main/java/win/bemtorres/lindasonrisaproveedor/MainActivity.kt
@@ -0,0 +1,12 @@
+package win.bemtorres.lindasonrisaproveedor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
